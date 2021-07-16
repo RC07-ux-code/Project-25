305855411793572bfba5a4889ff39defcd8a5963
RC07-ux-code <76210482+RC07-ux-code@users.noreply.github.com>
2021-07-16 11:47:50 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -48,7 +48,7 @@ function draw() {
 function keyPressed() {
 	if (keyCode === UP_ARROW) {
 
-	  Matter.Body.applyForce(paperObj.body,paperObj.body.position,{x:130,y:-145});
+	  Matter.Body.applyForce(paperObj.body,paperObj.body.position,{x:85,y:-90});
   
 	}
 }
